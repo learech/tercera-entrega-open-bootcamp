0c04f822ab6ea80cd71763f8a0de213a75b35681
Lea Rech <learech@hotmail.com>
2022-12-23 16:51:29 -0300

    subiendo

---
+++ factorial-break.js
@@ -0,0 +1,9 @@
+let i = 0;
+let max = 15;
+while (i < max) {
+    i++;
+    if (i === 11) {
+        break
+    }
+    console.log(i);
+}
---
+++ factorial-while.js
@@ -0,0 +1,6 @@
+let i = 0;
+let numero = 10;
+while (i <= numero) {
+    console.log(i);
+    i++
+}
